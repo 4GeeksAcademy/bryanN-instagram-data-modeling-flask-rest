22aff1ee9ed7f92b592e2786ae357d16c6e07d10
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-29 03:52:09 +0000

    finished tables, still to add relations

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -7,10 +7,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     firstname: Mapped[str] = mapped_column(String(120), nullable=False)
     lastname: Mapped[str] = mapped_column(String(120), nullable=False)
-
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
     def serialize(self):
         return {
@@ -18,21 +17,45 @@ class User(db.Model):
             "username": self.username,
             "email": self.email,
             "firstname": self.firstname,
-            "lastname": self.lastname,
-            # do not serialize the password, its a security breach
+            "lastname": self.lastname
         }
-    
+
+
 class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     commenttext: Mapped[str] = mapped_column(String(120), nullable=False)
-    author_id: Mapped[str] = mapped_column(ForeignKey('user.id'), nullable=False)
-    post_id: Mapped[str] = mapped_column(String(120), nullable=False)
-
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "commenttext": self.commenttext,
-            "post_id": self.post_id,
-            # do not serialize the password, its a security breach
+            "post_id": self.post_id
+        }
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id
+        }
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(String(120), nullable=False)
+    url: Mapped[str] = mapped_column(String(120), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id
         }
