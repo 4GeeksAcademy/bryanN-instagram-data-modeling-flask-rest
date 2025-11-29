03c902d8aa0f8f5f07d595995c264b94e81a782b
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-29 03:55:36 +0000

    finished tables, still to add relations

--- src/models.py
+++ src/models.py
@@ -4,6 +4,16 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+class Follower(db.Model):
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'),primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'),primary_key=True)
+
+    def serialize(self):
+        return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
